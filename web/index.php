e2ab881509ca20f9de7fc1ee82c53d9c1dcec1f3
gedongdong <gedongdong2010@163.com>
2018-10-15 14:55:21 +0800

    增加相关目录

--- web/index.php
+++ web/index.php
@@ -8,8 +8,8 @@ $conn = new mysqli($servername, $username, $password);
  
 // 检测连接
 if ($conn->connect_error) {
-    die("连接失败: " . $conn->connect_error);
+    die("mysql连接失败: " . $conn->connect_error);
 } 
-echo "连接成功";
+echo "mysql连接成功";
 echo '<hr>';
 phpinfo();
